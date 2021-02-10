31affc14c434188123b828f43d64c92093602229
enoch-strok <enochstrok@outlook.com>
2021-02-10 15:41:59 -0800

    First CS project. Complete.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Using Console.WriteLine, you can output any string to the terminal/console" + 3.14159);
+        
+
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+
+
+
+        };
+        }
+
+        
+    }
+}
